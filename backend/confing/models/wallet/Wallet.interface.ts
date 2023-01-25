66134d5dc57608c952eb75417d4f6c1f442d4b64
Jhonatan Tabraj <jhonntantb@gmail.com>
2023-01-25 11:58:03 -0500

    change model to js

--- backend/confing/models/wallet/Wallet.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Wallet extends Document {
-  readonly balance: number;
-  readonly address: string;
-  readonly coin: string;
-  readonly chainId: number,
-  readonly  transaction: string[];
-}
